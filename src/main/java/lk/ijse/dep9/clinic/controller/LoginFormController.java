f03fcec6926278653a0867b7c2f8a78d37a9de30
manelka <nishanmanelka@gmail.com>
2022-09-05 13:21:07 +0530

    Implement Login Logic

--- src/main/java/lk/ijse/dep9/clinic/controller/LoginFormController.java
+++ src/main/java/lk/ijse/dep9/clinic/controller/LoginFormController.java
@@ -4,7 +4,16 @@ import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
+import javafx.stage.Stage;
+import lk.ijse.dep9.clinic.security.SecurityContextHolder;
+import lk.ijse.dep9.clinic.security.User;
+import lk.ijse.dep9.clinic.security.UserRole;
+
+import java.io.IOException;
+import java.sql.*;
 
 public class LoginFormController {
     public JFXTextField txtUserName;
@@ -15,7 +24,7 @@ public class LoginFormController {
         btnLogin.setDefaultButton(true);
     }
 
-    public void btnLoginOnAction(ActionEvent actionEvent) {
+    public void btnLoginOnAction(ActionEvent actionEvent) throws ClassNotFoundException, IOException {
         String username = txtUserName.getText();
         String password = txtPassword.getText();
 
@@ -31,11 +40,50 @@ public class LoginFormController {
             return;
         }
 
-        if (username.matches("^[A-Za-z0-9]+$")){
+        if (!username.matches("^[A-Za-z0-9]+$")){
             new Alert(Alert.AlertType.ERROR,"Invalid login credential").show();
             txtUserName.requestFocus();
             txtUserName.selectAll();
             return;
+        }
+
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        try(Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/medical_clinic", "root", "Kasuni@1234")){
+            String sql = "SELECT role FROM User WHERE username='%s' AND password='%s'";
+            sql= String.format(sql,username,password);
+
+            Statement stm = connection.createStatement();
+            ResultSet rst = stm.executeQuery(sql);
+
+            if (rst.next()){
+                String role = rst.getString("role");
+                SecurityContextHolder.setPrincipal(new User(username,role));
+                Scene scene;
+                switch (role){
+                    case "Admin":
+                        scene = new Scene(FXMLLoader.load(this.getClass().getResource("/view/AdminDashboardForm.fxml")));
+                        break;
+                    case "Doctor":
+                        scene = new Scene(FXMLLoader.load(this.getClass().getResource("/view/DoctorDashboardForm.fxml")));
+                        break;
+                    default:
+                        scene = new Scene(FXMLLoader.load(this.getClass().getResource("/view/ReceptionistDashboardForm.fxml")));
+                }
+
+                Stage stage = new Stage();
+                stage.setScene(scene);
+                stage.show();
+                stage.centerOnScreen();
+                System.out.println(SecurityContextHolder.getPrincipal().toString());
+
+            }else {
+                new Alert(Alert.AlertType.ERROR,"Invalid login credential").show();
+                txtUserName.requestFocus();
+                txtUserName.selectAll();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            new Alert(Alert.AlertType.ERROR,"Failed to connect with the database, Try Again").show();
         }
 
     }
---
+++ src/main/java/lk/ijse/dep9/clinic/security/User.java
@@ -0,0 +1,38 @@
+package lk.ijse.dep9.clinic.security;
+
+public class User {
+    private String name;
+    private String role;
+
+    public User() {
+    }
+
+    public User(String name, String role) {
+        this.name = name;
+        this.role = role;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", role='" + role + '\'' +
+                '}';
+    }
+}
