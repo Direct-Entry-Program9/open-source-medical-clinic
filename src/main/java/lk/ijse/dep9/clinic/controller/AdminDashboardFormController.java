47e73a338664b42f2cf34860a8012fc0515f7162
manelka <nishanmanelka@gmail.com>
2022-09-05 16:59:25 +0530

    Close #7

--- src/main/java/lk/ijse/dep9/clinic/controller/AdminDashboardFormController.java
+++ src/main/java/lk/ijse/dep9/clinic/controller/AdminDashboardFormController.java
@@ -1,4 +1,19 @@
 package lk.ijse.dep9.clinic.controller;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+
 public class AdminDashboardFormController {
+    public JFXButton btnProfileManage;
+    public JFXButton btnViewRecords;
+    public JFXButton btnSettings;
+
+    public void btnProfileManageOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnViewRecordsOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnSettingsOnAction(ActionEvent actionEvent) {
+    }
 }
